0ed10d15b304618495b4a49dfc0afb5be4c8443f
Nadine Hernandez <nrhernandez93@gmail.com>
2020-04-16 13:07:15 -0400

    seperated components into smaller pieces and added some fuctions to app js

--- client/src/components/location.js
+++ client/src/components/location.js
@@ -1,13 +1,33 @@
 import React from 'react';
 
-function Location() {
+export function LocationCard(props) {
     return (
-        <div className="location">
-            <div className="risk-level-title"><h3>Risk Level: <span className="risk-level"></span></h3></div>
-            <div className="address"></div>
-            <div className="recommendation"></div>
+        <div className="location" {...props}>
+            {props.children}
         </div>
-    );
+    )
 }
 
-export default Location;
+export function RiskTitle(props) {
+    return (
+        <span className="risk-level" {...props}>
+            {props.children}
+        </span>
+    )
+}
+
+export function Address(props) {
+    return (
+        <div className="address">
+            {props.children}
+        </div>
+    )
+}
+
+export function Recommendation(props) {
+    return (
+        <div className="recommendation">
+            {props.children}
+        </div>
+    )
+}
