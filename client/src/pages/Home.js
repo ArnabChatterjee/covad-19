e39a20cddc8518d35a3dfb87278b1a7b61845381
Nadine Hernandez <nrhernandez93@gmail.com>
2020-04-16 22:45:12 -0400

    created stand in data for demo purposes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import {Form, Input, FormBtn} from '../components/search';
+import { Form, Input, FormBtn } from '../components/search';
 import { RiskTitle, Address, Recommendation, LocationCard } from '../components/location';
-import API from '../services/baseWebApi';
+import baseWebApi from '../services/baseWebApi';
 
 class App extends Component {
   state = {
@@ -10,70 +10,84 @@ class App extends Component {
   }
 
   handleInputChange = event => {
-    const value  = event.target.value;
+    const value = event.target.value;
     this.setState({ input: value });
   }
 
   handleFormSubmit = event => {
     event.preventDefault();
-
     if (this.state.input) {
-        API
-        .getLocationsRisk({
-            locations: this.state.input
-        })
-        .then(res => {
-            this.setState({
-            locations: res.data
-            });
-        })
-        .catch(error => {
-            let errorCode = error.status;
-            if (errorCode !== 401 && errorCode >= 399 && errorCode < 500){
-                errorCode = 400;
-            }
-            switch (errorCode) {
-            case 401:
-            case 400:
-                window.location.assign(`/`);//should make an error page if have time
-                break;
-            default:
-                window.location.assign(`/`);
-            }
-            console.log(error);
-        }
-      );
+      // let res = baseWebApi.getLocationsRisk(
+      //     this.state.input, "my location"
+      //   )
+        // .then(res => {
+        //     this.setState({
+        //     locations: res.data
+        //     });
+        //     // console.log(this.state.locations[0].title);
+        // })
+        // .catch(error => {
+        //   let errorCode = error.status;
+        //   if (errorCode !== 401 && errorCode >= 399 && errorCode < 500) {
+        //     errorCode = 400;
+        //   }
+        //   switch (errorCode) {
+        //     case 401:
+        //     case 400:
+        //       window.location.assign(`/`);//should make an error page if have time
+        //       break;
+        //     default:
+        //       window.location.assign(`/`);
+        //   }
+        //   console.log(error);
+        // }
+        // )
+      this.setState({
+        locations: [{
+          "title":"Walmart",
+          "address": "1871 Chamblee Tucker Rd, Chamblee, GA 30341",
+          "recommendation":"watson recommendation placeholder",
+          "risk":"Medium"
+        }]
+      });
     }
   };
 
   render() {
-  return (
-    <div className="app">
-      <div className="container">
-      <div className="search">
+    return (
+      <div className="app">
+        <div className="container">
+          <div className="search">
             <h2 className="search-title">Where are you going?</h2>
             <Form>
-                <Input 
+              <Input
+                value={this.state.input}
                 onChange={this.handleInputChange}>
-                </Input>
-                <FormBtn 
+              </Input>
+              <FormBtn
+                type="submit"
                 onClick={this.handleFormSubmit}>
-                </FormBtn>
+              </FormBtn>
             </Form>
-        </div>
+          </div>
 
-        {(this.state.locations).length > 0? (this.state.locations).map(location => 
-            <LocationCard>
-                <div className="risk-level-title">
-                    <h3>Risk Level: <RiskTitle>{location.risk}</RiskTitle></h3>
-                </div>
-                <Address>{location.address}</Address>
-                <Recommendation>{location.categoryName}</Recommendation>
+          {this.state.locations.length ? (
+            <div>
+            {this.state.locations.map(location => (
+            <LocationCard key={location.address}>
+              <div className="risk-level-title">
+                <h3>Risk Level: <RiskTitle>{location.risk}</RiskTitle></h3>
+              </div>
+              <div className="location-title">{location.title}</div>
+              <Address>{location.address}</Address>
+              <Recommendation>{location.recommendation}</Recommendation>
             </LocationCard>
-        ): (<div></div>)}
+          ))}
+          </div>
+     ) : (<div></div>)}
+        </div>
       </div>
-    </div>
-  );
+    );
   }
 }
 
